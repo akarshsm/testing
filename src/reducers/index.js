a142e7d0d45ecf75437869e6deb4204dd2e719ea
akarshsm <akarshsm@gmail.com>
2018-08-22 11:31:19 +0530

    redux-added action creators and linked to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { compineReducer, combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 import commentsReducer from 'reducers/comments';
 
 export default combineReducers ({
